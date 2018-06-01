ea3cf33555efac71e41e361a802e8414a56217fc
Mikael Arguedas <mikael@osrfoundation.org>
2018-06-01 14:28:01 +0200

    Rebase from 'release/melodic/cartographer_ros'

--- cartographer_ros/node_constants.h
+++ cartographer_ros/node_constants.h
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 
-#ifndef CARTOGRAPHER_ROS_NODE_CONSTANTS_H_
-#define CARTOGRAPHER_ROS_NODE_CONSTANTS_H_
+#ifndef CARTOGRAPHER_ROS_CARTOGRAPHER_ROS_NODE_CONSTANTS_H
+#define CARTOGRAPHER_ROS_CARTOGRAPHER_ROS_NODE_CONSTANTS_H
 
 #include <string>
 #include <vector>
@@ -28,6 +28,8 @@ constexpr char kMultiEchoLaserScanTopic[] = "echoes";
 constexpr char kPointCloud2Topic[] = "points2";
 constexpr char kImuTopic[] = "imu";
 constexpr char kOdometryTopic[] = "odom";
+constexpr char kNavSatFixTopic[] = "fix";
+constexpr char kLandmarkTopic[] = "landmark";
 constexpr char kFinishTrajectoryServiceName[] = "finish_trajectory";
 constexpr char kOccupancyGridTopic[] = "map";
 constexpr char kScanMatchedPointCloudTopic[] = "scan_matched_points2";
@@ -36,6 +38,7 @@ constexpr char kSubmapQueryServiceName[] = "submap_query";
 constexpr char kStartTrajectoryServiceName[] = "start_trajectory";
 constexpr char kWriteStateServiceName[] = "write_state";
 constexpr char kTrajectoryNodeListTopic[] = "trajectory_node_list";
+constexpr char kLandmarkPosesListTopic[] = "landmark_poses_list";
 constexpr char kConstraintListTopic[] = "constraint_list";
 constexpr double kConstraintPublishPeriodSec = 0.5;
 
@@ -48,4 +51,4 @@ std::vector<std::string> ComputeRepeatedTopicNames(const std::string& topic,
 
 }  // namespace cartographer_ros
 
-#endif  // CARTOGRAPHER_ROS_NODE_CONSTANTS_H_
+#endif  // CARTOGRAPHER_ROS_CARTOGRAPHER_ROS_NODE_CONSTANTS_H
